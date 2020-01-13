97d66880405b6847af9929830bb185c28cec1bcb
Daniel Andersen <daniel.andersen@alexandra.dk>
2020-01-13 07:58:36 +0100

    Initial commit

---
+++ spec/language-gnes-basic-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageGnesBasicView from '../lib/language-gnes-basic-view';
+
+describe('LanguageGnesBasicView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
